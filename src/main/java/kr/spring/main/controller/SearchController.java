c5beef8a0b6f990f3573711d88e47d35e2eb25de
user <user@DESKTOP-Q6OKFRU>
2024-02-14 15:01:59 +0900

    +

--- src/main/java/kr/spring/main/controller/SearchController.java
+++ src/main/java/kr/spring/main/controller/SearchController.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Controller
 public class SearchController {
 	@RequestMapping("/get.do")
@@ -28,12 +30,69 @@ public class SearchController {
 		
 		return EntityUtils.toString(response.getEntity());
 	}
-	/*
+	
 	@RequestMapping("/put.do")
 	@ResponseBody
 	public String put() throws IOException{
-		RestClient restClient = RestClient.builder(
-				);
-	}*/
+		
+		RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200, "http"),
+												   new HttpHost("localhost", 9201, "http")).build();
+		
+		Request request = new Request("PUT", "/movie_execute/_doc/1");
+		
+		Map<String,String> map = new HashMap<String, String>();
+		map.put("message", "elastic");
+		
+		ObjectMapper mapper = new ObjectMapper();
+		String body = mapper.writeValueAsString(map);
+		
+		request.setJsonEntity(body);
+		
+		Response response = restClient.performRequest(request);
+		
+		restClient.close();
+		
+		return EntityUtils.toString(response.getEntity());
+	}
+	
+	@RequestMapping("/post.do")
+	@ResponseBody
+	public String post() throws IOException{
+		
+		RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200, "http"),
+												   new HttpHost("localhost", 9201, "http")).build();
+		Request request = new Request("POST", "/movie2/_doc/1");
+		
+		Map<String,String> map = new HashMap<String,String>();
+		map.put("movieCd",  "3");
+		map.put("movieNm", "극한직업");
+		map.put("movieNmEn", "Extreme Job");
+		map.put("prdtYear", "2019");
+		map.put("openDt", "2019-01-23");
+		map.put("typeNm", "장편");
+		map.put("prdtStatNm", "기타");
+		map.put("nationAlt",  "한국");
+		map.put("genreAlt", "코미디");
+		map.put("repNationNm", "한국");
+		map.put("repGenreNm", "코미디");
+		
+		ObjectMapper mapper = new ObjectMapper();
+		String body = mapper.writeValueAsString(map);
+		
+		request.setJsonEntity(body);
+		
+		Response response = restClient.performRequest(request);
+		restClient.close();
+		
+		return EntityUtils.toString(response.getEntity());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
